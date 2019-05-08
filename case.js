2af56351757437883b03196dd628ef38410b1243
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-08 17:54:40 +0200

    basic files

---
+++ case.js
@@ -0,0 +1,15 @@
+// Galeria zdjęć
+// Wizualizacja galerii w pliku case.jpg
+// Macie za zadanie stworzyć galerię zdjęć. 
+// Galeria powinna na starcie wczytywać kilka przykładowych zdjęć i ich opisów z plików. 
+// Powinien być jeden plik json z danymi zdjęć i odpowiednia ilość plików jpg/png.
+// Opisy i tytuły można edytować, karty zdjęć można dodawać i usuwać.
+// Zdjęcie po kliknięciu powinno się powiększać dwukrotnie(bądź dowolną ilość razy).
+// Strona posiada tryb jasny i ciemny.
+// Style wedle uznania, ważne aby było widać różnicę między trybami.
+// Przycisk RESET przywraca stronę do stanu początkowego, bez odświeżenia.
+// Licznik zdjęć w galerii powinien się zmieniać w zależności od ilości zdjęć
+// Select obok resetu powinien zawierać aktualną listę tytułów zdjęć,
+//  i po wybraniu jednego z tytułów tylko to zdjęcie ma zostać w galerii.
+// Przycisk edytuj na kartach zdjęć ma odblokowywać i blokować 
+// możliwość edycji tytułu i opisu danego zdjęcia
